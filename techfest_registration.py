ea14e1f9f46753a21b331307b5fc69c76628209c
Guiuan <12410907@benilde.edu.ph>
2025-11-06 09:58:54 +0800

    Added commands to collect participant information.

--- techfest_registration.py
+++ techfest_registration.py
@@ -5,11 +5,24 @@ participants = []
 
 #task 1: registration setup
 print("Welcome to SMIT TechFest!\nOrganized by Trini Andrea Guiuan of APPDAET BTCS1")
-participants_count = int(input("How many participants will register? "))
-if participants_count.isdigit():
-    participants_count = int(participants_count)
-    if participants_count <= 0:
-        print("Invalid number of participants.")
-        exit()
-else:
-    print("Invalid input. Please enter a number!\n")
+
+participants_count = int(input("\nHow many participants will register? "))
+if participants_count <= 0:
+    print("Invalid number of participants.")
+    exit()
+
+#task 2: collect participant information
+for i in range(participants_count):
+    name = input("Enter participant name: ")
+    track = input("Enter chosen track: ")
+
+    participants_info = {
+        "name": name,
+        "track": track
+    }
+
+    participants.append(participants_info)
+
+print("\nRegistered Participants:")
+for i in range(len(participants)):
+    print(f"{i + 1}. {participants[i]['name']} - {participants[i]['track']}")
