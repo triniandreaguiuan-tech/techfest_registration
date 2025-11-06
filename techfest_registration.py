c1d7f3e7836cf9ede3b451b41b362ac8d0ae6701
Guiuan <12410907@benilde.edu.ph>
2025-11-06 10:23:30 +0800

    Added track diversity report, no display yet. Will display at Task 5.

--- techfest_registration.py
+++ techfest_registration.py
@@ -26,3 +26,20 @@ for i in range(participants_count):
 print("\nRegistered Participants:")
 for i in range(len(participants)):
     print(f"{i + 1}. {participants[i]['name']} - {participants[i]['track']}")
+
+#task 3: track diversity report
+print("\nTracks offered in this event: \nCybersecurity, Data, Game Dev, Information\n")
+csec = 0
+data = 0
+gameDev = 0
+inform = 0
+
+for p in participants:
+    if p["track"] == "Cybersecurity":
+        csec += 1
+    elif p["track"] == "Data":
+        data += 1
+    elif p["track"] == "Game Dev":
+        gameDev += 1
+    elif p["track"] == "Information":
+        inform += 1
