9f7a057b5b2ce03039637973ab1b128568bf7214
Guiuan <12410907@benilde.edu.ph>
2025-11-06 09:37:13 +0800

    Implemented SMIT TechFest registration setup.

--- techfest_registration.py
+++ techfest_registration.py
@@ -1,2 +1,15 @@
 # GUIUAN, Trini Andrea - APPDAET Midterms
 # Fully committed to git! :)
+
+participants = []
+
+#task 1: registration setup
+print("Welcome to SMIT TechFest!\nOrganized by Trini Andrea Guiuan of APPDAET BTCS1")
+participants_count = int(input("How many participants will register? "))
+if participants_count.isdigit():
+    participants_count = int(participants_count)
+    if participants_count <= 0:
+        print("Invalid number of participants.")
+        exit()
+else:
+    print("Invalid input. Please enter a number!\n")
